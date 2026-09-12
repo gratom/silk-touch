c6f2347018764d1d120e058c157a30f0d09ee04f
Gratom <gratomov@gmail.com>
2026-09-12 03:16:21 +0300

    add #editor + make configs

--- tools/DependencyResolver/SilkTouchDependencyResolver.cs
+++ tools/DependencyResolver/SilkTouchDependencyResolver.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.PackageManager;
@@ -5,196 +7,236 @@ using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
-[InitializeOnLoad]
-public class SilkTouchDependencyResolver : EditorWindow
+namespace Tools.DependencyResolver
 {
-    private class Dependency
+    [InitializeOnLoad]
+    public class SilkTouchDependencyResolver : EditorWindow
     {
-        public string Name;
-        public string PackageName;
-        public string InstallSource;
-        public bool IsInstalled;
-        public bool IsPackage;
-    }
-
-    private static readonly List<Dependency> dependencies = new List<Dependency>
-    {
-        new Dependency
+        private class Dependency
         {
-            Name = "UniTask",
-            PackageName = "com.cysharp.unitask",
-            InstallSource = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask",
-            IsPackage = true
-        },
-        new Dependency
-        {
-            Name = "Unity UI (TMP)",
-            PackageName = "com.unity.ugui",
-            InstallSource = "com.unity.ugui",
-            IsPackage = true
+            public string Name;
+            public string PackageName;
+            public string InstallSource;
+            public bool IsInstalled;
+            public bool IsPackage;
         }
-    };
 
-    private static ListRequest listRequest;
-    private static AddRequest addRequest;
-    private static int currentIndex = -1;
-    private static bool isInstalling;
-
-    static SilkTouchDependencyResolver()
-    {
-        EditorApplication.delayCall += CheckPackages;
-    }
-
-    private static void CheckPackages()
-    {
-        listRequest = Client.List(true);
-        EditorApplication.update += CheckListProgress;
-    }
-
-    private static void CheckListProgress()
-    {
-        if (listRequest != null && listRequest.IsCompleted)
+        private static readonly List<Dependency> dependencies = new List<Dependency>
         {
-            EditorApplication.update -= CheckListProgress;
-
-            if (listRequest.Status == StatusCode.Success)
+            new Dependency
             {
-                bool missingDependencies = false;
+                Name = "UniTask",
+                PackageName = "com.cysharp.unitask",
+                InstallSource = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask",
+                IsPackage = true
+            },
+            new Dependency
+            {
+                Name = "Unity UI (TMP)",
+                PackageName = "com.unity.ugui",
+                InstallSource = "com.unity.ugui",
+                IsPackage = true
+            }
+        };
 
-                for (int i = 0; i < dependencies.Count; i++)
+        private static ListRequest listRequest;
+        private static AddRequest addRequest;
+        private static int currentIndex = -1;
+        private static bool isInstalling;
+
+        static SilkTouchDependencyResolver()
+        {
+            EditorApplication.delayCall += CheckWrapper;
+        }
+
+        private static void CheckWrapper()
+        {
+            CheckPackages(false);
+        }
+
+        private static void CheckPackages(bool force)
+        {
+            if (SilkTouchEditorConfig.instance.IgnoreDependencyCheck && !force)
+            {
+                return;
+            }
+            listRequest = Client.List(true);
+            EditorApplication.update += CheckListProgress;
+        }
+
+        private static void CheckListProgress()
+        {
+            if (listRequest != null && listRequest.IsCompleted)
+            {
+                EditorApplication.update -= CheckListProgress;
+
+                if (listRequest.Status == StatusCode.Success)
                 {
-                    dependencies[i].IsInstalled = false;
-                    foreach (PackageInfo package in listRequest.Result)
+                    bool missingDependencies = false;
+                    for (int i = 0; i < dependencies.Count; i++)
                     {
-                        if (package.name == dependencies[i].PackageName)
+                        dependencies[i].IsInstalled = false;
+                        foreach (PackageInfo package in listRequest.Result)
                         {
-                            dependencies[i].IsInstalled = true;
-                            break;
+                            if (package.name == dependencies[i].PackageName)
+                            {
+                                dependencies[i].IsInstalled = true;
+                                break;
+                            }
+                        }
+
+                        if (!dependencies[i].IsInstalled)
+                        {
+                            missingDependencies = true;
                         }
                     }
 
-                    if (!dependencies[i].IsInstalled)
+                    if (missingDependencies)
                     {
-                        missingDependencies = true;
+                        ShowWindow();
                     }
                 }
 
-                if (missingDependencies)
+                listRequest = null;
+            }
+        }
+
+        [MenuItem("SilkTouch/Dependencies")]
+        public static void CheckAndShowWindow()
+        {
+            CheckPackages(true);
+        }
+
+        public static void ShowWindow()
+        {
+            SilkTouchDependencyResolver window = GetWindow<SilkTouchDependencyResolver>("Silk Touch Setup");
+            window.minSize = new Vector2(400, 200);
+            window.maxSize = new Vector2(400, 200);
+            window.Show();
+        }
+
+        private void OnDestroy()
+        {
+            if (!IsAllDependenciesInstalled())
+            {
+                SilkTouchEditorConfig.instance.IgnoreDependencyCheck = true;
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(15);
+            EditorGUILayout.HelpBox("Silk Touch requires additional dependencies.\nPlease install missing packages to ensure full functionality.",
+                MessageType.Warning
+            );
+
+            GUILayout.Space(10);
+
+            for (int i = 0; i < dependencies.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(dependencies[i].Name, GUILayout.Width(120));
+
+                if (dependencies[i].IsInstalled)
                 {
-                    ShowWindow();
+                    GUI.color = Color.green;
+                    EditorGUILayout.LabelField("[Installed]");
+                    GUI.color = Color.white;
+                }
+                else
+                {
+                    GUI.color = Color.red;
+                    EditorGUILayout.LabelField("[Missing]");
+                    GUI.color = Color.white;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            GUILayout.Space(10);
+
+            GUI.enabled = !isInstalling;
+
+            if (!isInstalling)
+            {
+                if (GUILayout.Button("Install Missing", GUILayout.Height(35)))
+                {
+                    InstallNextDependency();
                 }
             }
 
-            listRequest = null;
-        }
-    }
+            GUI.enabled = true;
 
-    public static void ShowWindow()
-    {
-        SilkTouchDependencyResolver window = GetWindow<SilkTouchDependencyResolver>("Silk Touch Setup");
-        window.minSize = new Vector2(400, 200);
-        window.maxSize = new Vector2(400, 200);
-        window.Show();
-    }
-
-    private void OnGUI()
-    {
-        GUILayout.Space(15);
-        EditorGUILayout.HelpBox("Silk Touch requires additional dependencies.\nPlease install missing packages to ensure full functionality.",
-            MessageType.Warning
-        );
-
-        GUILayout.Space(10);
-
-        for (int i = 0; i < dependencies.Count; i++)
-        {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(dependencies[i].Name, GUILayout.Width(120));
-
-            if (dependencies[i].IsInstalled)
+            if (isInstalling)
             {
-                GUI.color = Color.green;
-                EditorGUILayout.LabelField("[Installed]");
-                GUI.color = Color.white;
+                EditorGUILayout.HelpBox("Installing dependencies... Please, wait.", MessageType.Info);
+            }
+        }
+
+        private static void InstallNextDependency()
+        {
+            currentIndex = -1;
+            for (int i = 0; i < dependencies.Count; i++)
+            {
+                if (!dependencies[i].IsInstalled)
+                {
+                    currentIndex = i;
+                    break;
+                }
+            }
+
+            if (currentIndex != -1)
+            {
+                isInstalling = true;
+                Debug.Log($"[Silk Touch] Starting installation: {dependencies[currentIndex].Name}...");
+                addRequest = Client.Add(dependencies[currentIndex].InstallSource);
+                EditorApplication.update += InstallProgress;
             }
             else
             {
-                GUI.color = Color.red;
-                EditorGUILayout.LabelField("[Missing]");
-                GUI.color = Color.white;
-            }
-            EditorGUILayout.EndHorizontal();
-        }
-
-        GUILayout.Space(10);
-
-        GUI.enabled = !isInstalling;
-
-        if (!isInstalling)
-        {
-            if (GUILayout.Button("Install Missing", GUILayout.Height(35)))
-            {
-                InstallNextDependency();
-            }
-        }
-
-        GUI.enabled = true;
-
-        if (isInstalling)
-        {
-            EditorGUILayout.HelpBox("Installing dependencies... Please, wait.", MessageType.Info);
-        }
-    }
-
-    private static void InstallNextDependency()
-    {
-        currentIndex = -1;
-        for (int i = 0; i < dependencies.Count; i++)
-        {
-            if (!dependencies[i].IsInstalled)
-            {
-                currentIndex = i;
-                break;
-            }
-        }
-
-        if (currentIndex != -1)
-        {
-            isInstalling = true;
-            Debug.Log($"[Silk Touch] Starting installation: {dependencies[currentIndex].Name}...");
-            addRequest = Client.Add(dependencies[currentIndex].InstallSource);
-            EditorApplication.update += InstallProgress;
-        }
-        else
-        {
-            isInstalling = false;
-            if (HasOpenInstances<SilkTouchDependencyResolver>())
-            {
-                GetWindow<SilkTouchDependencyResolver>().Close();
-            }
-            AssetDatabase.Refresh();
-        }
-    }
-
-    private static void InstallProgress()
-    {
-        if (addRequest != null && addRequest.IsCompleted)
-        {
-            EditorApplication.update -= InstallProgress;
-
-            if (addRequest.Status == StatusCode.Success)
-            {
-                Debug.Log($"[Silk Touch] Successfully installed: {addRequest.Result.packageId}");
-                dependencies[currentIndex].IsInstalled = true;
-                InstallNextDependency();
-            }
-            else if (addRequest.Status >= StatusCode.Failure)
-            {
-                Debug.LogError($"[Silk Touch] Failed to install {dependencies[currentIndex].Name}: {addRequest.Error.message}");
                 isInstalling = false;
+                if (HasOpenInstances<SilkTouchDependencyResolver>())
+                {
+                    GetWindow<SilkTouchDependencyResolver>().Close();
+                }
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private static void InstallProgress()
+        {
+            if (addRequest != null && addRequest.IsCompleted)
+            {
+                EditorApplication.update -= InstallProgress;
+
+                if (addRequest.Status == StatusCode.Success)
+                {
+                    Debug.Log($"[Silk Touch] Successfully installed: {addRequest.Result.packageId}");
+                    dependencies[currentIndex].IsInstalled = true;
+                    InstallNextDependency();
+                }
+                else if (addRequest.Status >= StatusCode.Failure)
+                {
+                    Debug.LogError($"[Silk Touch] Failed to install {dependencies[currentIndex].Name}: {addRequest.Error.message}");
+                    isInstalling = false;
+                }
+
+                addRequest = null;
+            }
+        }
+
+        private static bool IsAllDependenciesInstalled()
+        {
+            for (int i = 0; i < dependencies.Count; i++)
+            {
+                if (!dependencies[i].IsInstalled)
+                {
+                    return false;
+                }
             }
 
-            addRequest = null;
+            return true;
         }
     }
 }
+
+#endif
---
+++ tools/DependencyResolver/SilkTouchEditorConfig.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Tools.DependencyResolver
+{
+    [FilePath("ProjectSettings/SilkTouchEditorConfig.asset", FilePathAttribute.Location.ProjectFolder)]
+    public class SilkTouchEditorConfig : ScriptableSingleton<SilkTouchEditorConfig>
+    {
+        [SerializeField] private bool ignoreDependencyCheck;
+
+        public bool IgnoreDependencyCheck
+        {
+            get => ignoreDependencyCheck;
+            set
+            {
+                ignoreDependencyCheck = value;
+                Save(true);
+            }
+        }
+    }
+}
+
+#endif
